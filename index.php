c7cf61871a2a2f3c832006e3bb03ebb13c001e8a
comkiter <jatupongt@mitrphol.com>
2016-12-29 01:54:13 +0700

    Comkiter

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 $access_token = 'w8Km+wvo6CWbnJ6Bk2T0A2w4N9SbhctqJJQXzoJo9RCNQcbPvcqsT3vmzafvPJzAlL5ZcoOYriQXu/ce4XWGK9Lj/yVqO3BRGY/cCH6YrRhsWgyZZH08XnB0u94OX2tIngYxglm0jI8TVp8IcqRJwQdB04t89/1O/w1cDnyilFU=';
 
@@ -5,6 +6,28 @@ $access_token = 'w8Km+wvo6CWbnJ6Bk2T0A2w4N9SbhctqJJQXzoJo9RCNQcbPvcqsT3vmzafvPJz
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
+// Make a POST Request to Messaging API to reply to sender
+$messages = [
+				'type' => 'text',
+				'text' => 'ทำไมต้องกวนฉันด้วย+++++++++++++++'
+			];                        
+$url = 'https://api.line.me/v2/bot/message/reply';
+                        $data = [
+                                'replyToken' => ,$access_token
+                                'messages' => [$messages],
+                        ];
+                        $post = json_encode($data);
+                        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                        $ch = curl_init($url);
+                        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                        $result = curl_exec($ch);
+                        curl_close($ch);
+                        echo $result . "\r\n";
+/*
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -44,4 +67,5 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
+*/
 echo "OK";
