94bd8eb4c33221a615597b88e58aa6e1eef68c39
comkiter <jatupongt@mitrphol.com>
2016-12-29 01:13:37 +0700

    der

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?
-echo "Helllo World TEST";
+<?php
+$access_token = 'w8Km+wvo6CWbnJ6Bk2T0A2w4N9SbhctqJJQXzoJo9RCNQcbPvcqsT3vmzafvPJzAlL5ZcoOYriQXu/ce4XWGK9Lj/yVqO3BRGY/cCH6YrRhsWgyZZH08XnB0u94OX2tIngYxglm0jI8TVp8IcqRJwQdB04t89/1O/w1cDnyilFU=';
+
+$url = 'https://api.line.me/v1/oauth/verify';
+
+$headers = array('Authorization: Bearer ' . $access_token);
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
