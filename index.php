5c72bb33e28d40c5f602f246b955c80c47257b51
comkiter <jatupongt@mitrphol.com>
2016-12-29 11:16:24 +0700

    Comkiter

--- index.php
+++ index.php
@@ -19,8 +19,14 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
+				{
 				'type' => 'text',
 				'text' => 'ทำไมต้องกวนฉันด้วย'
+				},
+				{
+				'type' => 'text',
+				'text' => 'ทำไมต้องกวนฉันด้วย++++'
+				}
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
