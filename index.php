3b47b7e81784250cbbbd7f4a5a2e0df1ecbf1b9d
comkiter <jatupongt@mitrphol.com>
2016-12-29 11:59:33 +0700

    Comkiter

--- index.php
+++ index.php
@@ -48,8 +48,7 @@ if (!is_null($events['events'])) {
 // Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
-				'type' => 'text',
-				'text' => 'ทำไมต้องกวนฉันด้วย'
+				'to' => 'ทำไมต้องกวนฉันด้วย'
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
